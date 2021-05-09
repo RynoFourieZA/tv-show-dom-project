2eac24312a433b1afed041fd53cba8be9e2a6d41
Ryno <43918267+RynoFourieZA@users.noreply.github.com>
2021-05-09 19:11:32 +0200

    Level 100
    Added episode name, season number, episode number, medium image and summary. Also add a footer tag on the bottom of the page.

--- script.js
+++ script.js
@@ -6,7 +6,40 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  
+  for (let episode of getAllEpisodes()) {
+
+    let tvShowName = document.createElement("h2");
+    tvShowName.innerText = `${episode.name} - S${episode.season.toString().padStart(2, 0)}E${episode.number.toString().padStart(2, 0)}`; // First used back ticks, use string interpolation then target object and its property, get an array convert to string, use padStart it adds 2 digits
+    rootElem.appendChild(tvShowName);
+    
+    let tvShowImg = document.createElement("img");
+    tvShowImg.src = episode.image.medium;
+    rootElem.appendChild(tvShowImg);
+    
+    let tvShowSummary = document.createElement("p");
+    tvShowSummary.innerHTML = episode.summary;
+    rootElem.appendChild(tvShowSummary);
+    
+  }
+
+  let siteFooter = document.createElement("footer");
+  console.log(siteFooter);
+  document.body.appendChild(siteFooter);
+  let footerParagraph = document.createElement("p");
+  let link = document.createElement("A");
+  link.setAttribute("href", "https://www.tvmaze.com/");
+  link.innerHTML = "Tv show";
+  footerParagraph.innerText = `The data displayed on this page was originally taken from: `;
+  
+  siteFooter.appendChild(footerParagraph);
+
+
+  footerParagraph.appendChild(link);
+  
+  
 }
 
 window.onload = setup;
