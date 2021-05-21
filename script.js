370b9591aa6c0c34f4a7e3d0d1af087c4183b986
Ryno <rjrfourie@outlook.com>
2021-05-21 15:57:15 +0200

    Level 200
    made variables out side of the functions so it can be called globally. Made small functions to use it effectively.

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
+// Get all episodes globally
+const allEpisodes = getAllEpisodes();
+const blockCards = document.getElementById("block-cards");
+
+
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
+  console.log(episodeList);
+
+allEpisodes.forEach(episode => addEpisode(episode));
   
-  for (let episode of getAllEpisodes()) {
-
-    let tvShowName = document.createElement("h2");
-    tvShowName.innerText = `${episode.name} - S${episode.season.toString().padStart(2, 0)}E${episode.number.toString().padStart(2, 0)}`; // First used back ticks, use string interpolation then target object and its property, get an array convert to string, use padStart it adds 2 digits
-    rootElem.appendChild(tvShowName);
-    
-    let tvShowImg = document.createElement("img");
-    tvShowImg.src = episode.image.medium;
-    rootElem.appendChild(tvShowImg);
-    
-    let tvShowSummary = document.createElement("p");
-    tvShowSummary.innerHTML = episode.summary;
-    rootElem.appendChild(tvShowSummary);
-    
-  }
-
+  // footer function 
   let siteFooter = document.createElement("footer");
   console.log(siteFooter);
   document.body.appendChild(siteFooter);
@@ -35,11 +26,45 @@ function makePageForEpisodes(episodeList) {
   footerParagraph.innerText = `The data displayed on this page was originally taken from: `;
   
   siteFooter.appendChild(footerParagraph);
-
-
+  
   footerParagraph.appendChild(link);
   
   
 }
 
-window.onload = setup;
+function addEpisode(episode) {
+
+  let tvShowName = document.createElement("h2");
+  tvShowName.innerText = `${episode.name} - S${episode.season.toString().padStart(2, 0)}E${episode.number.toString().padStart(2, 0)}`; // First used back ticks, use string interpolation then target object and its property, get an array convert to string, use padStart it adds 2 digits
+  blockCards.appendChild(tvShowName);
+  
+  let tvShowImg = document.createElement("img");
+  tvShowImg.src = episode.image.medium;
+  blockCards.appendChild(tvShowImg);
+  
+  let tvShowSummary = document.createElement("p");
+  tvShowSummary.innerHTML = episode.summary;
+  blockCards.appendChild(tvShowSummary);
+
+}
+
+// Search Function
+let gmCharacters = [];
+const searchBar = document.getElementById("searchBar"); 
+
+    // here we needed to see if the input value is showing.
+    searchBar.addEventListener("keyup", (e) => {
+      blockCards.innerHTML = " ";
+      const searchString = e.target.value.toLowerCase();
+      console.log("result: ",searchString);
+
+      const filteredCharacters = allEpisodes.filter( (character) => { 
+        return (
+          character.name.toLowerCase().includes(searchString) || character.summary.toLowerCase().includes(searchString));
+        })
+        console.log(filteredCharacters);
+        filteredCharacters.forEach(episode => addEpisode(episode));
+      }); 
+    
+    
+    window.onload = setup;
