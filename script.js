6f6b534ee92787f945bc62e34ebb4ac342aa77e0
Ryno <rjrfourie@outlook.com>
2021-07-23 16:48:39 +0200

    add select shows

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Get all episodes globally
+// Get all shows globally
+const shows = getAllShows();
+console.log("my shows: ", shows);
 const blockCards = document.getElementById("block-cards");
 const tvShowForm = document.getElementById("form");
 
@@ -16,12 +18,12 @@ fetch("https://api.tvmaze.com/shows/82/episodes")
     // do whatever you want with the JSON response
     // console.log(data);
     makePageForEpisodes(data);
-  })
-  // .catch((error) => {
-  //   // Handle the error
-  //   console.log(error);
-  // });
-  
+  });
+// .catch((error) => {
+//   // Handle the error
+//   console.log(error);
+// });
+
 //You can edit ALL of the code here
 // function setup(allEpisodes) {
 
@@ -105,7 +107,6 @@ function tvShowSearchBar(allEpisodes) {
     let charactersLength = filteredCharacters.length;
     console.log(charactersLength);
     episodeDisplay.innerText = `Displaying ${charactersLength}/73 episodes`;
-
   });
 }
 
@@ -122,3 +123,50 @@ function tvShowFooter() {
   footerParagraph.appendChild(link);
 }
 
+//Select input of all shows
+function selectShows(shows) {
+  let selectInput = document.createElement("select");
+  tvShowForm.appendChild(selectInput);
+  let optionInput = document.createElement("option");
+  optionInput.innerText = "Show all Shows";
+  optionInput.value = "";
+  selectInput.appendChild(optionInput);
+  shows.forEach((show) => {
+    let optionInput = document.createElement("option");
+    optionInput.innerText = `${show.name}`;
+    selectInput.appendChild(optionInput);
+    optionInput.value = show.name;
+  });
+
+  selectInput.addEventListener("change", (e) => {
+    blockCards.innerHTML = " ";
+    const inputSelect = e.target.value;
+    const filteredShows = shows.filter((show) => {
+      if (inputSelect === show.id) {
+        console.log("you clicked me");
+      } else {
+        null;
+      }
+    });
+
+    // filteredShows.forEach((show) => addShow(show));
+  });
+}
+
+// Add Shows
+// function addShow(shows) {
+//       let tvShowName = document.createElement("h2");
+//       tvShowName.innerText = `${shows.name}`;
+//       blockCards.appendChild(tvShowName);
+//       let tvShowImg = document.createElement("img");
+//       tvShowImg.src = shows.forEach(show => console.log(show.image.medium));
+//       blockCards.appendChild(tvShowImg);
+
+//       let tvShowSummary = document.createElement("p");
+//       tvShowSummary.innerHTML = shows.summary;
+//       blockCards.appendChild(tvShowSummary);
+//     };
+
+// Call my shows function
+selectShows(shows);
+// addShow(shows);
