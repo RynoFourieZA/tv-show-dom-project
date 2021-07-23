964e030edc4da6861485acfa524b9938f9483797
Ryno <rjrfourie@outlook.com>
2021-07-23 18:15:33 +0200

    get the list of shows

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 // Get all shows globally
 const shows = getAllShows();
-console.log("my shows: ", shows);
 const blockCards = document.getElementById("block-cards");
 const tvShowForm = document.getElementById("form");
 
-fetch("https://api.tvmaze.com/shows/82/episodes")
-  .then((response) => {
-    if (response.status >= 200 && response.status <= 299) {
-      return response.json();
-    } else {
-      throw new Error(
-        `Encountered something unexpected: ${response.status} ${response.statusText}`
-      );
-    }
-  })
-  .then((data) => {
-    // do whatever you want with the JSON response
-    // console.log(data);
-    makePageForEpisodes(data);
+//Select input of all shows
+function selectShows(shows) {
+  let selectInput = document.createElement("select");
+  tvShowForm.appendChild(selectInput);
+  let optionInput = document.createElement("option");
+  optionInput.innerText = "Show all Shows";
+  optionInput.value = "";
+  selectInput.appendChild(optionInput);
+  shows.forEach((show) => {
+    let optionInput = document.createElement("option");
+    optionInput.innerText = `${show.name}`;
+    selectInput.appendChild(optionInput);
+    optionInput.value = show.name || show.id;
   });
-// .catch((error) => {
-//   // Handle the error
-//   console.log(error);
-// });
 
-//You can edit ALL of the code here
-// function setup(allEpisodes) {
+  selectInput.addEventListener("change", (e) => {
+    blockCards.innerHTML = " ";
+    const inputSelect = e.target.value;
+    console.log("This is my select event: ", inputSelect);
+    found = shows.find((show) => {
+      if (show.name === inputSelect) {
+        return show.id;
+      }
+    });
 
-//   makePageForEpisodes(allEpisodes);
+    fetch(`https://api.tvmaze.com/shows/${found.id}/episodes`)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          throw new Error(
+            `Encountered something unexpected: ${response.status} ${response.statusText}`
+          );
+        }
+      })
+      .then((data) => {
+        // do whatever you want with the JSON response
+        // console.log(data);
+        makePageForEpisodes(data);
+      });
+  });
+}
 
-// }
+selectShows(shows);
 
 function makePageForEpisodes(allEpisodes) {
   selectTvShowInput(allEpisodes);
@@ -123,50 +140,3 @@ function tvShowFooter() {
   footerParagraph.appendChild(link);
 }
 
-//Select input of all shows
-function selectShows(shows) {
-  let selectInput = document.createElement("select");
-  tvShowForm.appendChild(selectInput);
-  let optionInput = document.createElement("option");
-  optionInput.innerText = "Show all Shows";
-  optionInput.value = "";
-  selectInput.appendChild(optionInput);
-  shows.forEach((show) => {
-    let optionInput = document.createElement("option");
-    optionInput.innerText = `${show.name}`;
-    selectInput.appendChild(optionInput);
-    optionInput.value = show.name;
-  });
-
-  selectInput.addEventListener("change", (e) => {
-    blockCards.innerHTML = " ";
-    const inputSelect = e.target.value;
-    const filteredShows = shows.filter((show) => {
-      if (inputSelect === show.id) {
-        console.log("you clicked me");
-      } else {
-        null;
-      }
-    });
-
-    // filteredShows.forEach((show) => addShow(show));
-  });
-}
-
-// Add Shows
-// function addShow(shows) {
-//       let tvShowName = document.createElement("h2");
-//       tvShowName.innerText = `${shows.name}`;
-//       blockCards.appendChild(tvShowName);
-//       let tvShowImg = document.createElement("img");
-//       tvShowImg.src = shows.forEach(show => console.log(show.image.medium));
-//       blockCards.appendChild(tvShowImg);
-
-//       let tvShowSummary = document.createElement("p");
-//       tvShowSummary.innerHTML = shows.summary;
-//       blockCards.appendChild(tvShowSummary);
-//     };
-
-// Call my shows function
-selectShows(shows);
-// addShow(shows);
